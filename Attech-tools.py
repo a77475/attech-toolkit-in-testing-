26eb2ce8f7ab55919868500f539a3e9ea6d243fd
a77475 <160498711+a77475@users.noreply.github.com>
2024-03-27 18:45:08 +0100

    Attech-tools.py

--- Attech-tools.py
+++ Attech-tools.py
@@ -104,20 +104,4 @@ class IPToLocationWindow(tk.Toplevel):
         self.geometry("400x400")
         self.configure(bg="#f0f0f0")
 
-        ttk.Label(self, text="IP to Location Tool", font=("Helvetica", 20), background="#f0f0f0").pack(pady=20)
-
-        self.entry_ip = self.create_entry("IP Address:")
-
-        style = ThemedStyle(self)
-        style.set_theme("arc")
-
-    def create_entry(self, label_text, **kwargs):
-        frame = ttk.Frame(self, padding=(50, 0), style="Main.TFrame")
-        frame.pack(fill=tk.X, **kwargs)
-        ttk.Label(frame, text=label_text, background="#f0f0f0").pack(side=tk.LEFT)
-        entry = ttk.Entry(frame)
-        entry.pack(side=tk.RIGHT, fill=tk.X, expand=True)
-
-    def open_map(self):
-        ip_address = self.entry_ip.get()
-       
+        ttk.Label(self, text="IP to
